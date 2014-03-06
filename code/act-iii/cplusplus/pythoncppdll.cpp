1f9e0aefb8936fd420f0fd580339dcd0b369c2a3
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-03-06 13:19:04 -0800

    Add a status callback to the DLL

--- code/act-iii/cplusplus/pythoncppdll.cpp
+++ code/act-iii/cplusplus/pythoncppdll.cpp
@@ -5,65 +5,79 @@
 
 extern "C"
 {
-  __declspec(dllexport) int AddAreaFieldToFeatureClassCPlusPlus(const wchar_t* feature_class, const wchar_t* field_name)
+  __declspec(dllexport) int AddAreaFieldToFeatureClassCPlusPlus(const wchar_t* feature_class,
+                                                                const wchar_t* field_name,
+                                                                const void(*callback_function)(int))
   {
-	  // Convert wchar_t*s to bstring
-	  _bstr_t catalogPath(feature_class),
-	          newfieldname(field_name);
+    // Convert wchar_t*s to bstring
+    _bstr_t catalogPath(feature_class),
+            newfieldname(field_name);
 
-	  // Coinitialize GP utilities class
-	  IGPUtilitiesPtr ipUtil(CLSID_GPUtilities);
+    // Coinitialize GP utilities class
+    IGPUtilitiesPtr ipUtil(CLSID_GPUtilities);
 
-	  // Feature class holder
-	  IFeatureClassPtr ipFeatureclass(0);
+    // Feature class holder
+    IFeatureClassPtr ipFeatureclass(0);
 
-	  HRESULT hr;
+    HRESULT hr;
 
-	  // Try to fetch feature class from catalog path
-	  if (FAILED(hr = ipUtil->OpenFeatureClassFromString(catalogPath, &ipFeatureclass)))
-		  return -1;
+    // Try to fetch feature class from catalog path
+    if (FAILED(hr = ipUtil->OpenFeatureClassFromString(catalogPath, &ipFeatureclass)))
+      return -1;
 
-	  // Field index of the field of interest
-	  long fieldIndex;
-	  if (FAILED(ipFeatureclass->FindField(newfieldname, &fieldIndex)))
+    // Field index of the field of interest
+    long fieldIndex;
+    if (FAILED(ipFeatureclass->FindField(newfieldname, &fieldIndex)))
       return -2;
 
-	  // Set up query and filter
-	  IQueryFilterPtr ipFilter(CLSID_QueryFilter);
-	  IFeatureCursorPtr ipCursor;
-	  IFeaturePtr ipRow;
-	  IGeometryPtr ipShape;
+    // Set up query and filter
+    IQueryFilterPtr ipFilter(CLSID_QueryFilter);
+    IFeatureCursorPtr ipCursor;
+    IFeaturePtr ipRow;
+    IGeometryPtr ipShape;
 
-	  // Open cursor on feature class
-	  ipFeatureclass->Update(ipFilter, VARIANT_FALSE, &ipCursor);
+    // Open cursor on feature class
+    ipFeatureclass->Update(ipFilter, VARIANT_FALSE, &ipCursor);
 
-	  // Iterate
-	  esriGeometryType gt;
-	  for (ipCursor->NextFeature(&ipRow);
-		     ipRow != NULL;
-		     ipCursor->NextFeature(&ipRow))
-	  {
-		  // Get row's associated geometry
-		  ipRow->get_Shape(&ipShape);
-		  // Ensure we've got a polygon
-		  ipShape->get_GeometryType(&gt);
-		  if (gt != esriGeometryPolygon)
-			  return -3;
-		  // Get area
-		  IAreaPtr ipArea(ipShape);
-		  double area;
-		  ipArea->get_Area(&area);
-		  // Pop double into a variant
-		  VARIANT value;
-		  ::VariantInit(&value);
-		  value.vt = VT_R8;
-		  value.dblVal = area;
-		  // Set double variant onto target field
-		  ipRow->put_Value(fieldIndex, value);
-		  // Save
-		  ipRow->Store();
-	  }
+    // Row number
+    int rowNumber(0);
 
-	  return 0;
+    // Variant we're going to reuse for the area value
+    VARIANT value;
+    ::VariantInit(&value);
+
+    // Iterate
+    esriGeometryType gt;
+    for (ipCursor->NextFeature(&ipRow);
+         ipRow != NULL;
+         ipCursor->NextFeature(&ipRow))
+    {
+      // Get row's associated geometry
+      ipRow->get_Shape(&ipShape);
+      // Ensure we've got a polygon
+      ipShape->get_GeometryType(&gt);
+      if (gt != esriGeometryPolygon)
+        return -3;
+      // Get area
+      IAreaPtr ipArea(ipShape);
+      double area;
+      ipArea->get_Area(&area);
+      // Pop double into a variant
+      value.vt = VT_R8;
+      value.dblVal = area;
+      // Set double variant onto target field
+      ipRow->put_Value(fieldIndex, value);
+      // Save
+      ipRow->Store();
+      ::VariantClear(&value);
+
+      if (rowNumber % 100 == 0 && callback_function != NULL)
+      {
+        callback_function(rowNumber);
+      }
+      rowNumber++;
+    }
+
+    return 0;
   }
 }
--- code/act-iii/field_area_calculator.py
+++ code/act-iii/field_area_calculator.py
@@ -3,14 +3,27 @@ import os
 
 import arcpy
 
+CALLBACK_C_PROTOTYPE = ctypes.CFUNCTYPE(None, ctypes.c_int)
+
+class Callback(object):
+    def __init__(self, feature_layer):
+        self._row_count = int(arcpy.management.GetRowCount(feature_layer)[0])
+        arcpy.SetProgressor('step', "Calculating records", 0,
+                            self._row_count, 1)
+        self.c_function = CALLBACK_C_PROTOTYPE(self.update)
+    def update(self, i):
+        arcpy.SetProgressorPosition(i)
+
 def execute_tool(featureclass_path, field_name):
+
     loaded_dll = ctypes.cdll.LoadLibrary(
             os.path.join(
                 os.path.abspath(os.path.dirname(__file__)),
                 "pythoncppdll.dll"))
     calculate_area_field = loaded_dll.AddAreaFieldToFeatureClassCPlusPlus
 
-    calculate_area_field.argtypes = [ctypes.c_wchar_p, ctypes.c_wchar_p]
+    calculate_area_field.argtypes = [ctypes.c_wchar_p, ctypes.c_wchar_p,
+                                     ctypes.POINTER(CALLBACK_C_PROTOTYPE)]
     calculate_area_field.restype = ctypes.c_int
 
     arcpy.AddMessage("Adding field {0}".format(field_name))
@@ -19,7 +32,9 @@ def execute_tool(featureclass_path, field_name):
                               "NON_REQUIRED", "#")
 
     arcpy.AddMessage("Executing C++ function...")
-    returncode = calculate_area_field(featureclass_path, field_name)
+    callback = Callback(featureclass_path)
+    returncode = calculate_area_field(featureclass_path, field_name,
+                                      callback.c_function)
 
     if returncode == -1:
         arcpy.AddError("Error opening Feature Class")
